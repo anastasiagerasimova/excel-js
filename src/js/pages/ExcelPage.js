a36257b0af6d5e2044254cc26c36a7163f967f5a
Anastasia Gerasimova <anastasiagerasimova210295@gmail.com>
2020-09-06 16:55:45 +0300

    Implement DIP

--- src/js/pages/ExcelPage.js
+++ src/js/pages/ExcelPage.js
@@ -1,4 +1,4 @@
-import {Page} from '../core/Page';
+import {Page} from '../core/page/Page';
 import {Excel} from '../components/excel/Excel';
 import {Header} from '../components/header/Header';
 import {Toolbar} from '../components/toolbar/Toolbar';
@@ -6,27 +6,37 @@ import {Formula} from '../components/formula/Formula';
 import {Table} from '../components/table/Table';
 import {rootReducer} from '../redux/rootReducer';
 import {createStore} from '.././core/createStore';
-import {storage, debounce} from '../core/utils';
 import {normalizeInitialState} from '../redux/initialState';
-
-function storageName(param) {
-	return 'excel:' + param;
-}
+import {StateProcessor} from '../core/page/StateProcessor';
+import {LocalStorageClient} from '../shared/LocalStorageClient';
 
 export class ExcelPage extends Page {
 	constructor(params) {
 		super(params);
+		this.storeSub = null;
+
+		// new implementation, DIP
+		this.processor = new StateProcessor(
+			new LocalStorageClient(this.params)
+		);
 	}
 
-	getRoot() {
-		const state = storage(storageName(this.params));
+	async getRoot() {
+		// old implementation
+		// const state = storage(storageName(this.params));
+		// const initialState = normalizeInitialState(state);
+		// const store = createStore(rootReducer, initialState);
+
+		// const stateListener = debounce(state =>{
+		// 	storage(storageName(this.params), state);
+		// }, 300);
+		// this.storeSub = store.subscribe(stateListener);
+
+		// new implementation, DIP
+		const state = await this.processor.get();
 		const initialState = normalizeInitialState(state);
 		const store = createStore(rootReducer, initialState);
-
-		const stateListener = debounce(state =>{
-			storage(storageName(this.params), state);
-		}, 300);
-		store.subscribe(stateListener);
+		this.storeSub = store.subscribe(this.processor.listen);
 
 		this.excel = new Excel({
 			components: [Header, Toolbar, Formula, Table],
@@ -42,5 +52,6 @@ export class ExcelPage extends Page {
 
 	destroy() {
 		this.excel.destroy();
+		this.storeSub.unsubscribe();
 	}
 }
